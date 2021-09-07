20d76bc398bcd6e4f8f62980c3b07e5d7c060f28
Zillessen, Sebastian <sebastian.zillessen@zuehlke.com>
2021-09-07 16:47:38 +0200

    Add a configuration with bean for the third party library.

---
+++ src/main/java/ch/zuehlke/springfundamentals/dependencyinjection/config/LocalAndMockProfileAreActive.java
@@ -0,0 +1,15 @@
+package ch.zuehlke.springfundamentals.dependencyinjection.config;
+
+import org.springframework.context.annotation.Conditional;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Conditional(LocalAndMockProfileCondition.class)
+public @interface LocalAndMockProfileAreActive {
+
+}
---
+++ src/main/java/ch/zuehlke/springfundamentals/dependencyinjection/config/LocalAndMockProfileCondition.java
@@ -0,0 +1,18 @@
+package ch.zuehlke.springfundamentals.dependencyinjection.config;
+
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+public class LocalAndMockProfileCondition implements Condition {
+
+    @Override
+    public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+        List<String> activeProfiles = asList(context.getEnvironment().getActiveProfiles());
+        return activeProfiles.contains("local") && activeProfiles.contains("mock");
+    }
+}
