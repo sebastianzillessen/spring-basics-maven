32e044235faa0da1fe19750385a683472ebbdb30
Zillessen, Sebastian <sebastian.zillessen@zuehlke.com>
2021-09-09 14:24:03 +0200

    Fix wrong dependency injection in CustomerController

--- src/main/java/ch/zuehlke/springfundamentals/dependencyinjection/controller/CustomerController.java
+++ src/main/java/ch/zuehlke/springfundamentals/dependencyinjection/controller/CustomerController.java
@@ -1,28 +1,20 @@
 package ch.zuehlke.springfundamentals.dependencyinjection.controller;
 
-import ch.zuehlke.springfundamentals.dependencyinjection.dataaccess.CustomerLoader;
 import ch.zuehlke.springfundamentals.dependencyinjection.service.CustomerService;
-import ch.zuehlke.springfundamentals.dependencyinjection.service.EmailService;
-import ch.zuehlke.springfundamentals.dependencyinjection.service.PostalService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomerController {
 
-  private final EmailService emailService;
-  private final PostalService postalService;
-  private final CustomerLoader customerLoader;
+  private final CustomerService customerService;
 
   @Autowired
-  public CustomerController(EmailService emailService, PostalService postalService, CustomerLoader customerLoader) {
-    this.emailService = emailService;
-    this.postalService = postalService;
-    this.customerLoader = customerLoader;
+  public CustomerController(CustomerService customerService) {
+    this.customerService = customerService;
   }
 
   public void deactivateCustomerAccount(String customerId) {
-    CustomerService customerService = new CustomerService(emailService, postalService, customerLoader);
     customerService.deactivateCustomer(customerId);
   }
 }
